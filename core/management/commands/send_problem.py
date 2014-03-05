57e6c49b55c68a484f4552ea63d57d796a8de05f
Max Burstein <mburst@maxburstein.com>
2014-03-05 06:16:59 +0000

    Updated send_problem to Friday

--- core/management/commands/send_problem.py
+++ core/management/commands/send_problem.py
@@ -29,7 +29,7 @@ class Command(BaseCommand):
                 msg.send(fail_silently=True)
 
             #Send weekly on Friday (4)
-            if today.weekday() == 2:
+            if today.weekday() == 4:
                 monday = today-timedelta(days=5)
                 problems = Problem.objects.filter(date__range=(monday, today))
                 if problems and weekly_subscribers:
