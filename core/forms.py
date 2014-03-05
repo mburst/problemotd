36b49aef7caea26f26ca4fb328244077466d18c9
Max Burstein <mburst@maxburstein.com>
2014-03-05 07:22:20 +0000

    Restore comment on incorrect captcha

--- core/forms.py
+++ core/forms.py
@@ -10,6 +10,7 @@ class ProblemSuggestionForm(forms.ModelForm):
 
 class CommentForm(forms.ModelForm):
     ancestor = forms.CharField(widget=forms.HiddenInput(attrs={'class': 'ancestor'}), required=False)
+    text = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '6'}))
 
     class Meta:
         model = Comment
